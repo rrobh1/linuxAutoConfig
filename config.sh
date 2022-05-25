af7089fa24a0e3bd7bb78da736096f15941b6a89
Robson Rodrigues <robson.oliveira@direcional.com.br>
2022-05-24 23:33:20 -0300

    automação

---
+++ config.sh
@@ -0,0 +1,18 @@
+sudo apt-get install zsh
+sudo apt-get install git
+sudo apt-get install htop
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+wget https://github.com/dracula/tilix/archive/master.zip
+unzip master.zip
+
+mkdir ~/.config/tilix
+mkdir ~/.config/tilix/schemes
+
+mv tilix-master/Dracula.json ~/.config/tilix/schemes
+
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+yes | cp -rf .zshrc ~/.zshrc
