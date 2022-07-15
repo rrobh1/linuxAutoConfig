6e7658e951010d1cd582cb887af553dd3b1381aa
Robson Rodrigues <robson.oliveira@direcional.com.br>
2022-07-15 10:57:36 -0300

    teste

--- teste1.sh
+++ teste1.sh
@@ -1,5 +1,11 @@
-#!/bin/bash
-
-echo "Enter Your Name: "
-read x
-echo "Welcome ${x}!"
+while true; do
+  read -p "Do you wish to install this program? " yn
+  case $yn in
+  [Yy]*)
+    make install
+    break
+    ;;
+  [Nn]*) exit ;;
+  *) echo "Please answer yes or no." ;;
+  esac
+done
