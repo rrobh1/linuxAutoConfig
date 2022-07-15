9f4d57e77e633e5e36ca24f4fe8a8eade16d0261
Robson Rodrigues <robson.oliveira@direcional.com.br>
2022-07-15 10:53:39 -0300

    teste

--- teste1.sh
+++ teste1.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-PS3="Select a day (1-4): "
-select i in mon tue wed exit; do
-  case $i in
-  mon) echo "Monday" ;;
-  tue) echo "Tuesday" ;;
-  wed) echo "Wednesday" ;;
-  exit) exit ;;
-  esac
-done
+echo "Enter Your Name: "
+read x
+echo "Welcome ${x}!"
