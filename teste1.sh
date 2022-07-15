c1861a1b55ac54c166dc51402c32e6aa23654762
Robson Rodrigues <robson.oliveira@direcional.com.br>
2022-07-15 11:01:48 -0300

    teste

--- teste1.sh
+++ teste1.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-while true; do
-  read -p "Do you wish to install this program? " yn
-  case $yn in
-  [Yy]*)
-    make install
-    break
-    ;;
-  [Nn]*) exit ;;
-  *) echo "Please answer yes or no." ;;
-  esac
-done
+echo -n "Enter the name of an animal: "
+read ANIMAL
+echo -n "The $ANIMAL has "
+case $ANIMAL in
+horse | dog | cat) echo -n "four" ;;
+man | kangaroo) echo -n "two" ;;
+*) echo -n "an unknown number of" ;;
+esac
+echo " legs."
